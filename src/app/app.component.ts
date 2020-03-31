cfa53f03f7eb213410a2407f6b7b5cd21098a129
PratikHorizoncore <pratik.khunt@horizoncore.com>
2020-03-31 13:16:25 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,29 +44,73 @@ export class AppComponent {
   }
 
   appendResponseCard(botMessage, responseCard) {
-    const botli = this.renderer.createElement("li");
-    const botp = this.renderer.createElement("p");
-    this.renderer.addClass(botli, "bot-li");
-    this.renderer.addClass(botp, "bot-p");
-    const bottext = this.renderer.createText(`Bot: ${botMessage}`);
-    this.renderer.appendChild(botp, bottext);
-    this.renderer.appendChild(botli, botp);
 
-    for(let i = 0, len = this.statement6.responseCard.genericAttachments[0].buttons.length; i < len; i++) {
-      const buttons = this.statement6.responseCard.genericAttachments[0].buttons[i];
+    // li
+    const botli = this.renderer.createElement('li');
+    this.renderer.addClass(botli, "bot-li");
+
+    // li > div
+    const botlidiv = this.renderer.createElement('div');
+    this.renderer.addClass(botlidiv, "li-div");
+    this.renderer.appendChild(botli, botlidiv);
+
+    // li > div > p
+    const botp = this.renderer.createElement('p');
+    const botptext = this.renderer.createText(`Bot: ${botMessage}`);
+    this.renderer.appendChild(botp, botptext);
+    this.renderer.addClass(botp, "bot-p");
+    this.renderer.appendChild(botlidiv, botp);
+
+    // li > div > div
+    const botcard = this.renderer.createElement('div');
+    this.renderer.addClass(botcard, "card");
+    this.renderer.appendChild(botlidiv, botcard);
+
+    // li > div > div > cardtitle
+    const botcardtitle = this.renderer.createElement('div');
+    this.renderer.addClass(botcardtitle, "card-title");
+    this.renderer.appendChild(botcard, botcardtitle);
+
+    // li > div > div > cardtitle > span
+    const cardtitlespan = this.renderer.createElement('span');
+    const cardtitlespantext = this.renderer.createText(responseCard.genericAttachments[0].title);
+    this.renderer.appendChild(cardtitlespan, cardtitlespantext);
+    this.renderer.appendChild(botcardtitle, cardtitlespan);
+
+    // li > div > div > cardtext
+    const botcardtext = this.renderer.createElement('div');
+    this.renderer.addClass(botcardtext, "card-text");
+    this.renderer.appendChild(botcard, botcardtext);
+
+    // li > div > div > cardtext > span
+    const cardtextspan = this.renderer.createElement('span');
+    const cardtextspantext = this.renderer.createText(responseCard.genericAttachments[0].subTitle);
+    this.renderer.appendChild(cardtextspan, cardtextspantext);
+    this.renderer.appendChild(botcardtext, cardtextspan);
+
+    // li > div > div > cardmedia
+    const cardmedia = this.renderer.createElement('div');
+    this.renderer.addClass(cardmedia, "card-media");
+    this.renderer.appendChild(botcard, cardmedia);
+
+    // li > div > div > cardaction
+    const cardaction = this.renderer.createElement('div');
+    this.renderer.addClass(cardaction, 'card-actions');
+    this.renderer.appendChild(botcard, cardaction);
+
+    for(let i = 0, len = responseCard.genericAttachments[0].buttons.length; i < len; i++) {
+      const buttons = responseCard.genericAttachments[0].buttons[i];
       const button = this.renderer.createElement("button");
       const buttontext = this.renderer.createText(buttons.text);
       this.renderer.setProperty(button, "value", buttons.value);
       this.renderer.addClass(button, "response-card-button");
       this.renderer.appendChild(button, buttontext);
-      this.renderer.appendChild(botli, button);
-      // this.renderer.addClass(responseDiv, "response-card-div");
+      this.renderer.appendChild(cardaction, button);
       this.renderer.listen(button, "click", event => {
         console.log(event.target.value);
       })
     }
-    // this.renderer.addClass(responseDiv, "response-card-div");
-    // this.renderer.appendChild(botli, responseDiv);
+
     this.renderer.appendChild(this.animateThis.nativeElement, botli);
   }
 
@@ -90,7 +134,7 @@ export class AppComponent {
           ],
           imageUrl:
             "https://cdn5.f-cdn.com/contestentries/357874/17076392/56d3f7013ac78_thumb900.jpg",
-          subTitle: null,
+          subTitle: "test",
           title: "Appoinment Scheduler"
         }
       ],
