2e113e4336755d186a4f1dac8689fa5b36444081
PratikHorizoncore <pratik.khunt@horizoncore.com>
2020-03-31 10:51:28 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-          import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
+import { Component, ElementRef, Renderer2, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-component",
